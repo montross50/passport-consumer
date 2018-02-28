18f4dc09f2183db21e7aa6fcddee78250fccf797
Trent Schmidt <trent.schmidt@ark.org>
2018-02-28 12:18:42 -0600

    Naming the routes for easier reference.

--- src/routes.php
+++ src/routes.php
@@ -5,26 +5,41 @@ Route::group(['prefix'=>config('passport-consumer.route_prefix'),'middleware'=>c
 
     if (config('passport-consumer.enable_pg')) {
         $routeName = config('passport-consumer.route_name_pg');
-        Route::post($routeName.'/login', 'PassportConsumerController@login');
+        Route::post($routeName.'/login', [
+            'as' => 'pg_login',
+            'uses' => 'PassportConsumerController@login'
+        ]);
         Route::post($routeName.'/refresh', [
             'as' => 'pg_refresh',
             'uses'=>'PassportConsumerController@refresh']);
 
         Route::group(['middleware' => ['auth:api']], function () use ($routeName) {
-            Route::post($routeName. '/logout', 'PassportConsumerController@logout');
+            Route::post($routeName. '/logout', [
+                'as' => 'pg_logout',
+                'uses' => 'PassportConsumerController@logout'
+            ]);
         });
     }
 
     if (config('passport-consumer.enable_access')) {
         $routeName = config('passport-consumer.route_name_access');
-        Route::get($routeName.'/redirect', 'PassportConsumerController@redirect');
-        Route::get($routeName.'/callback', 'PassportConsumerController@callback');
+        Route::get($routeName.'/redirect', [
+            'as' => 'access_redirect',
+            'uses' => 'PassportConsumerController@redirect'
+        ]);
+        Route::get($routeName.'/callback', [
+            'as' => 'access_callback',
+            'uses' => 'PassportConsumerController@callback'
+        ]);
         Route::post($routeName.'/refresh', [
             'as' => 'access_refresh',
             'uses'=>'PassportConsumerController@refresh']);
 
         Route::group(['middleware' => ['auth:api']], function () use ($routeName) {
-            Route::post($routeName. '/logout', 'PassportConsumerController@logout');
+            Route::post($routeName. '/logout', [
+               'as' => 'access_logout',
+               'uses' => 'PassportConsumerController@logout'
+            ]);
         });
     }
 });
